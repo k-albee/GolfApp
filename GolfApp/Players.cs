ac61406e9111283f514889d6e9709e97940eac80
Kevin Albee <kalbee@microsoft.com>
2020-01-31 14:57:14 -0800

    initial commit

---
+++ GolfApp/Players.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolfApp
+{
+    class Players
+    {
+        public int PlayerNumber { get; set; }
+        public string PlayerName { get; set; }
+        public string Nickname { get; set; }
+        public decimal Handicap { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ GolfApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GolfApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
