aade7c54977c024c6a3ef79fafd052a62d7604c3
Kevin Albee <kalbee@microsoft.com>
2020-01-31 17:47:20 -0800

    updated class

--- GolfApp/Players.cs
+++ GolfApp/Players.cs
@@ -5,12 +5,60 @@ using System.Text;
 namespace GolfApp
 {
     class Players
-    {
-        public int PlayerNumber { get; set; }
+    {/// <summary>
+    /// This class is a Players profile
+    /// </summary>
+        #region Properties
         public string PlayerName { get; set; }
         public string Nickname { get; set; }
-        public decimal Handicap { get; set; }
         public string EmailAddress { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public int Holes { get; set; }
+        public int Score { get; set; }
+        public int Fairways { get; set; }
+        public int Putts { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Records the number of holes played
+        /// </summary>
+        /// <param name="holes">Holes played in a round</param>
+        /// <returns>Total holes played</returns>
+        public int PlayedGolf(int holes)
+        {
+            Holes += holes;
+            return Holes;
+        }
+        /// <summary>
+        /// Records the score of holes played
+        /// </summary>
+        /// <param name="score">Total score for a round</param>
+        /// <returns>Total score for all rounds played</returns>
+        public int ScoredGolf (int score)
+        {
+            Score += score;
+            return Score;
+        }
+        /// <summary>
+        /// Records the number of fairways hit for a round from the tee
+        /// </summary>
+        /// <param name="fairways">Total fairways hit for a round</param>
+        /// <returns>Total fairways hit for all rounds played</returns>
+        public int HitFairways (int fairways)
+        {
+            Fairways += fairways;
+            return Fairways;
+        }
+        /// <summary>
+        /// Records the number of putts hit for a round
+        /// </summary>
+        /// <param name="putts">Total putts hit for a round</param>
+        /// <returns>Total putts hit for all rounds played</returns>
+        public int HitPutts (int putts)
+        {
+            Putts += putts;
+            return Putts;
+        }
+        #endregion
     }
 }
